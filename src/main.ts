ff64169e579542a0d036d0990dff53bf243ee341
leeductai <ldtai26112002@gmail.com>
2023-10-02 16:31:10 +0700

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('MALT Store API')
     .setDescription('The MALT Store Backend API description')
