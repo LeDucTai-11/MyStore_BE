ff13ebbabeedacc0ecc7581187dc9d1dc3d8e480
leeductai <ldtai26112002@gmail.com>
2023-10-07 22:26:59 +0700

    fix: update miss key desciption category

--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsOptional } from "class-validator";
 
 export class CreateCategoryDTO {
     @ApiProperty({required: true, default: 'Đồ uống - Giải khát'})
     @IsNotEmpty()
     name: string;
 
-    @ApiProperty({required: false, default: 'Bao gồm những sản phẩm chuyên về đồ uống'})
+    @ApiProperty({default: 'Bao gồm những sản phẩm chuyên về đồ uống'})
+    @IsOptional()
     description?: string;
 }
