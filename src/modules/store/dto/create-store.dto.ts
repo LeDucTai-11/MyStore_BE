e8e60f0b40a13b7c25a23e6968f66d33df07f660
leeductai <ldtai26112002@gmail.com>
2023-10-24 15:48:35 +0700

    refactor: update flow create product store

--- src/modules/store/dto/create-store.dto.ts
+++ src/modules/store/dto/create-store.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateStoreDTO {
-  @ApiProperty({ required: true, default: 'Đồ uống - Giải khát' })
+  @ApiProperty({ required: true, default: 'Nguyễn Lương Bằng' })
   @IsNotEmpty()
   address: string;
 }
