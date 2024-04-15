afa73b1836bbfc467dcd2180b3901d7300935950
leeductai <ldtai26112002@gmail.com>
2024-04-15 23:36:38 +0700

    re-set up virtual machine azure

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to our Website, The MALT Store !';
+    return 'Welcome to our Website, The Duc Tai Store !';
   }
 }
